e07f03471d94f61b836caf21ef6badaf3dd7896e
MAizazJaved <maizazjaved@hotmail.com>
2019-07-11 11:42:34 +0500

    data base connections

--- routes/index.js
+++ routes/index.js
@@ -7,34 +7,99 @@ var formobj = {}
 
 
 
+/*data base connection*/
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+});
+
+
+//model created
+
+const fatherModel = mongoose.model('father', {
+    father_name: String,
+    father_nric: Number,
+    father_phone: Number,
+});
+
+
+
+
+
+
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 router.get('/new', function (req, res, next) {
-  res.render('newPage', { title: 'newPage' });
+    res.render('newPage', { title: 'newPage' });
 });
+
+/* text area  */
 
 router.post('/new', function (req, res, next) {
-  console.log(req.body.mytext);
-  listItems.push(req.body.mytext);
-  console.log(listItems);
-  res.send('text saved');
+    console.log(req.body.mytext);
+    listItems.push(req.body.mytext);
+    console.log(listItems);
+    res.send('text saved');
 })
 
+
 router.get('/about', function (req, res, next) {
-  res.render('page1', { text: listItems });
+    res.render('page1', { text: listItems });
 });
+
+
+/* index page to form inputs */
 
 router.post('/form', function (req, res, next) {
-  formobj = req.body
-  res.send('success')
+
+    formobj = req.body
+
+    // data entry
+
+    const fatherObj = new fatherModel
+        ({
+            father_name: req.body.father_name,
+            father_nric: req.body.father_nric,
+            father_phone: req.body.father_phone,
+        });
+
+    //added to data base
+
+    fatherObj.save().then(() => console.log('data stored'));
+    res.send('success')
+
 });
 
-router.get('/form', function (req, res, next) {
-  res.render('form', { form_data: formobj })
+router.get('/page2', function (req, res, next) {
+
+    // fathers.find({},function(err,data){
+    //   if(err) {
+
+    //   } else {
+
+    //   }
+    // })
+    fatherModel.findById("5d25966d747bd11e35c49a5a").then(function (data) {
+        console.log(data)
+        res.render('display', { form_data: data })
+    }).catch(function (err) {
+        console.log('error', err)
+    });
+
+
 });
 
+router.get('/design',function(req,res,next){
+    res.render('design');
+});
 
 module.exports = router;
